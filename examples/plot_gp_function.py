64d3632c7bd2a0ce9e457bb6f692bf0f21be7601
annaiob <92999290+annaiob@users.noreply.github.com>
2022-01-04 22:09:04 +0100

    update plot_convergence function

--- examples/plot_gp_function.py
+++ examples/plot_gp_function.py
@@ -100,75 +100,76 @@ def plot_gp(optimizer, x, target):
     
     
     
-def plot_convergence(optimizer, x, target):
+def plot_convergence(optimizer, x, target, kind):
     x = x.reshape(-1,1)
     y = target(x)
     
-    def suggest(utility, target, optimizer):
-        next_point = optimizer.suggest(utility)['x']
-        target = target(next_point)
+    #def suggest(utility, target, optimizer):
+        #next_point = optimizer.suggest(utility)
+        #target = target(**next_point)
         #optimizer.register(params=next_point, target=target)
     
-        return target, next_point
+        #return target, next_point
         #print(optimizer.max)
-    iter=5
-    target_ucb=np.zeros(iter)
-    point_ucb=np.zeros(iter)
-    target_ei=np.zeros(iter)
-    point_ei=np.zeros(iter)
-    target_poi=np.zeros(iter)
-    point_poi=np.zeros(iter)
-    i=0
-    for _ in range(iter):
-        utility_function_ucb = UtilityFunction(kind="ucb", kappa=5, xi=0)
-        utility_ucb = utility_function_ucb.utility(x, None, optimizer._gp, 0)
-        target_ucb[i], point_ucb[i] = suggest(utility_function_ucb, target, optimizer)
-    
-        utility_function_ei = UtilityFunction(kind="ei", kappa=5, xi=0)
-        utility_ei = utility_function_ei.utility(x, None, optimizer._gp, 0)
-        target_ei[i], point_ei[i] = suggest(utility_function_ei, target, optimizer)
-    
-        utility_function_poi = UtilityFunction(kind="poi", kappa=5, xi=0)
-        utility_poi = utility_function_poi.utility(x, None, optimizer._gp, 0)
-        target_poi[i], point_poi[i] = suggest(utility_function_poi, target, optimizer)
+    it=20
+    tar=np.zeros(it)
+    point=np.zeros(it)
+    opt=None
+    if kind=='kg':
+        opt=optimizer
         
-        i=i+1
-    
+    for i in range(it):
+        utility_function = UtilityFunction(kind=kind, kappa=5, xi=0)
+        utility = utility_function.utility(x, opt, optimizer._gp, 0)
+        point[i] = optimizer.suggest(utility_function)['x']
+        tar[i]= target(point[i])
+        optimizer.register(params=point[i], target=tar[i])
+      
+    #print(point)
+    #print(tar)
     #utility_function_kg = UtilityFunction(kind="kg", kappa=5, xi=0)
     #utility_kg = utility_function_kg.utility(x, optimizer, optimizer._gp, 0)
     #target_kg, point_kg = suggest(utility_function_kg, target, optimizer)
     
     fig = plt.figure(figsize=(25, 15))
-    gs = gridspec.GridSpec(3, 1) 
-    ucb_plt = plt.subplot(gs[0])
-    ei_plt = plt.subplot(gs[1])
-    poi_plt = plt.subplot(gs[2])
+    
+    acq = plt.subplot()
+    
     #kg_plt = plt.subplot(gs[])
     steps = len(optimizer.space)
     fig.suptitle(
-        'Chosen points After {} Steps of UCB, EI and POI'.format(steps),
+        'Chosen points After {} Steps '.format(steps),
         fontdict={'size':30}
     )
     
     # UCB
-    ucb_plt.plot(point_ucb, target_ucb, '*',markersize=15,markerfacecolor='purple', markeredgecolor='k', markeredgewidth=1,label='UCB')
-    ucb_plt.set_xlim((-30,30))
-    ucb_plt.set_ylim((min(min(utility_ucb), min(utility_ei), min(utility_poi)) - 0.5, max(max(utility_ucb), max(utility_ei), max(utility_poi)) + 0.5))
-    
+    acq.plot(point, tar, '*',markersize=15,markerfacecolor='purple', markeredgecolor='k', markeredgewidth=1,label='UCB')
+    acq.set_xlim((min(point)-0.5,max(point)+0.5))
+    acq.set_ylim((min(tar) - 0.5, max(tar) + 0.5))
+    acq.axvline(x=x[np.argmax(y)], linestyle=':')
+    for i in range(it):
+        acq.annotate(str(i+1), # this is the text
+                     (point[i],tar[i]), # these are the coordinates to position the label
+                     textcoords="offset points",
+                     xytext=(0,10),# how to position the text
+                     ha='center',
+                     size=30) # horizontal alignment can be left, right or center
     #acq.plot(x[np.argmax(utility_ucb)], np.max(utility_ucb), '*', markersize=15, markerfacecolor='purple', markeredgecolor='k', markeredgewidth=1)
-    
+    """  
     # EI
     ei_plt.plot(point_ei, target_ei, '*',markersize=15,markerfacecolor='green', markeredgecolor='k', markeredgewidth=1,label='EI')
-    ei_plt.set_xlim((-30,30))
+    ei_plt.set_xlim((-10,10))
     ei_plt.set_ylim((min(min(utility_ucb), min(utility_ei), min(utility_poi)) - 0.5, max(max(utility_ucb), max(utility_ei), max(utility_poi)) + 0.5))
- 
+    ei_plt.axvline(x=x[np.argmax(y)], linestyle=':')
+     
     #acq.plot(x[np.argmax(utility_ei)], np.max(utility_ei), '*', markersize=15, markerfacecolor='green', markeredgecolor='k', markeredgewidth=1)
     
     # Poi
     poi_plt.plot(point_poi, target_poi, '*',markersize=15,markerfacecolor='blue', markeredgecolor='k', markeredgewidth=1,label='POI')
-    poi_plt.set_xlim((-30,30))
+    poi_plt.set_xlim((-10,10))
     poi_plt.set_ylim((min(min(utility_ucb), min(utility_ei), min(utility_poi)) - 0.5, max(max(utility_ucb), max(utility_ei), max(utility_poi)) + 0.5))
-    
+    poi_plt.axvline(x=x[np.argmax(y)], linestyle=':')
+    """
     plt.show()
 
     #acq.plot(x[np.argmax(utility_poi)], np.max(utility_poi), '*', markersize=15, markerfacecolor='blue', markeredgecolor='k', markeredgewidth=1)
